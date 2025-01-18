e6d24213ba7c86ec1a156e18eb3875b4e10ef888
johnrowley <outsidethebox@eircom.net>
2025-01-18 12:55:20 +0000

    Added Edit and Delete

--- Controllers/ExpenseController.cs
+++ Controllers/ExpenseController.cs
@@ -21,14 +21,44 @@ public class ExpenseController : Controller
         return View(expenses);
     }
     
-    public IActionResult CreateEditExpense()
+    public IActionResult CreateEditExpense(int? Id)
     {
        _logger.LogInformation("Create Expense");
-        return View();
+       if (Id != null)
+       {
+           var item = _context.Expenses.Find(Id);
+           return View(item);
+       }
+       return View();
+    }
+    
+    public IActionResult DeleteExpense(int Id)
+    {
+        _logger.LogInformation("Delete Expense");
+        var item = _context.Expenses.Find(Id);
+        
+        if (item != null)
+        {
+            _context.Expenses.Remove(item);
+            _context.SaveChanges();
+        }
+       
+        return RedirectToAction("Index");
     }
     public IActionResult CreateEditExpenseForm(Expense model)
     {
-        _context.Expenses.Add(model);
+        if (model.Id == 0)
+        {
+            _logger.LogInformation($"Creating Expense {model.Id}: {model.Description}");
+            _context.Expenses.Add(model);
+          
+           
+        }
+        else
+        {
+            _logger.LogInformation($"Updating Expense {model.Id}: {model.Description}");
+            _context.Expenses.Update(model);
+        }
         _context.SaveChanges();
         _logger.LogInformation($"Saving Expense {model.Id}: {model.Description}");
         return RedirectToAction("Index");
--- Models/Expense.cs
+++ Models/Expense.cs
@@ -4,6 +4,6 @@ public class Expense
 {
     public int Id { get; set; }
     
-    public string Description { get; set; } = string.Empty;
-    public int Value { get; set; }
+    public string? Description { get; set; } = string.Empty;
+    public decimal Value { get; set; }
 }
